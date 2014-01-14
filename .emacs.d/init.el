22659dafa5fd387828abb838f5940564bd9b53cf
Yusei Nishiyama <allegllet.scherzand@gmail.com>
2014-01-14 15:39:14 +0900

    Disable multi-term

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,26 +45,26 @@
 
 ;;;;;;;;;;;;;;;;;;;;Font;;;;;;;;;;;;;;;;;;;;
 
-(set-face-attribute 'default nil
-            :family "Menlo" ;; font 
-           :height 140)    ;; font size
+;; (set-face-attribute 'default nil
+;;             :family "Menlo" ;; font 
+;;            :height 140)    ;; font size
 
-(set-fontset-font
- nil 'japanese-jisx0208
- (font-spec :family "Hiragino Mincho Pro")) ;; font
+;; (set-fontset-font
+;;  nil 'japanese-jisx0208
+;;  (font-spec :family "Hiragino Mincho Pro")) ;; font
 
-(setq face-font-rescale-alist
-      '((".*Hiragino_Mincho_pro.*" . 1.2)))
+;; (setq face-font-rescale-alist
+;;       '((".*Hiragino_Mincho_pro.*" . 1.2)))
 
 ;;;;;;;;;;;;;;;;;;;;Window;;;;;;;;;;;;;;;;;;;;
 
 ;; 画面分割
 (add-hook 'after-init-hook (lambda()
     (setq w (selected-window))
-    (setq w2 (split-window-horizontally))
+    (setq w2 (split-window-vertically (floor (* 0.7 (window-height)))))
     (select-window w2)
-    (multi-term)
-    (setq w3 (split-window-vertically (floor (* 0.2 (window-height)))))
+;;    (multi-term)
+;;    (setq w3 (split-window-vertically (floor (* 0.2 (window-height)))))
     (run-scheme scheme-program-name)
     (select-window w)))
 
@@ -116,55 +116,55 @@
 
 ;;;;;;;;;;;;;;;;;;;;Shell;;;;;;;;;;;;;;;;;;;;
 
-;; より下に記述した物が PATH の先頭に追加されます
-(dolist (dir (list
-              "/sbin"
-              "/usr/sbin"
-              "/bin"
-              "/usr/bin"
-              "/opt/local/bin"
-              "/sw/bin"
-              "/usr/local/bin"
-              (expand-file-name "~/bin")
-              (expand-file-name "~/.emacs.d/bin")
-              ))
+;; ;; より下に記述した物が PATH の先頭に追加されます
+;; (dolist (dir (list
+;;               "/sbin"
+;;               "/usr/sbin"
+;;               "/bin"
+;;               "/usr/bin"
+;;               "/opt/local/bin"
+;;               "/sw/bin"
+;;               "/usr/local/bin"
+;;               (expand-file-name "~/bin")
+;;               (expand-file-name "~/.emacs.d/bin")
+;;               ))
 
-;; PATH と exec-path に同じ物を追加します
- (when (and (file-exists-p dir) (not (member dir exec-path)))
-   (setenv "PATH" (concat dir ":" (getenv "PATH")))
-   (setq exec-path (append (list dir) exec-path))))
-(setenv "MANPATH" (concat "/usr/local/man:/usr/share/man:/Developer/usr/share/man:/sw/share/man" (getenv "MANPATH")))
+;; ;; PATH と exec-path に同じ物を追加します
+;;  (when (and (file-exists-p dir) (not (member dir exec-path)))
+;;    (setenv "PATH" (concat dir ":" (getenv "PATH")))
+;;    (setq exec-path (append (list dir) exec-path))))
+;; (setenv "MANPATH" (concat "/usr/local/man:/usr/share/man:/Developer/usr/share/man:/sw/share/man" (getenv "MANPATH")))
 
-;; shell の存在を確認
-(defun skt:shell ()
-  (or (executable-find "zsh")
-      (executable-find "bash")
-      ;; (executable-find "f_zsh") ;; Emacs + Cygwin を利用する人は Zsh の代りにこれにしてください
-      ;; (executable-find "f_bash") ;; Emacs + Cygwin を利用する人は Bash の代りにこれにしてください
-      (executable-find "cmdproxy")
-      (error "can't find 'shell' command in PATH!!")))
+;; ;; shell の存在を確認
+;; (defun skt:shell ()
+;;   (or (executable-find "zsh")
+;;       (executable-find "bash")
+;;       ;; (executable-find "f_zsh") ;; Emacs + Cygwin を利用する人は Zsh の代りにこれにしてください
+;;       ;; (executable-find "f_bash") ;; Emacs + Cygwin を利用する人は Bash の代りにこれにしてください
+;;       (executable-find "cmdproxy")
+;;       (error "can't find 'shell' command in PATH!!")))
 
-;; shell 名の設定
-(setq shell-file-name (skt:shell))
-(setenv "SHELL" shell-file-name)
-(setq explicit-shell-file-name shell-file-name)
+;; ;; shell 名の設定
+;; (setq shell-file-name (skt:shell))
+;; (setenv "SHELL" shell-file-name)
+;; (setq explicit-shell-file-name shell-file-name)
 
 ;; multi-termのロード。
-(setq multi-term-program "/bin/zsh")
-(require 'multi-term)
+;; (setq multi-term-program "/bin/zsh")
+;; (require 'multi-term)
 
 ;; multi-term内でのショートカットを設定する。
-(when (require 'multi-term nil t)
-   (add-hook 'term-mode-hook
-         '(lambda ()
-            ;; C-h を term 内文字削除にする
-            (define-key term-raw-map (kbd "C-h") 'term-send-backspace)
-            ;; C-y を term 内ペーストにする
-            (define-key term-raw-map (kbd "C-y") 'term-paste)
-            )))
+;; (when (require 'multi-term nil t)
+;;    (add-hook 'term-mode-hook
+;;          '(lambda ()
+;;             ;; C-h を term 内文字削除にする
+;;             (define-key term-raw-map (kbd "C-h") 'term-send-backspace)
+;;             ;; C-y を term 内ペーストにする
+;;             (define-key term-raw-map (kbd "C-y") 'term-paste)
+;;             )))
 
-;; Emacs が保持する terminfo を利用する
-(setq system-uses-terminfo nil)
+;; ;; Emacs が保持する terminfo を利用する
+;; (setq system-uses-terminfo nil)
 
 ;;;;;;;;;;;;;;;;;;;;Evernote;;;;;;;;;;;;;;;;;;;;
 
