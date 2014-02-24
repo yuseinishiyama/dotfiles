a2cc02575275547084d6c9e225c07749a42d353c
nishiyama_y <allegllet.scherzand@gmail.com>
2014-02-25 08:12:13 +0900

    can toggle truncate lines

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -78,6 +78,8 @@
 (global-set-key "\C-h" 'delete-backward-char)
 ;; ウィンドウ切り替え
 (define-key global-map (kbd "C-t") 'other-window)
+;; 折り返しトグルコマンド
+(define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
 
 ;;;;;;;;;;;;;;;;;;;; Font ;;;;;;;;;;;;;;;;;;;;
 
