b6868b30de5194d414f06b0fb05456d17bc79926
nishiyama_y <allegllet.scherzand@gmail.com>
2014-04-27 23:47:37 +0900

    Workaround for errors of haskell-mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -265,10 +265,12 @@
 (add-to-list 'auto-mode-alist '("\\.lhs$" . literate-haskell-mode))
 (add-to-list 'auto-mode-alist '("\\.cabal\\'" . haskell-cabal-mode))
 
-(add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 
 (autoload 'ghc-init "ghc" nil t)
-(add-hook 'haskell-mode-hook (lambda() (ghc-init)))
+;; (ghc-init)
+;; (add-hook 'haskell-mode-hook (lambda() (ghc-init)))
 
 ;;;;;;;;;;;;;;;;;;;; Scheme ;;;;;;;;;;;;;;;;;;;;
 
