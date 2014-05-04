9b2669c06ef8faf9a876dfea7d848fac9e9619bf
nishiyama_y <allegllet.scherzand@gmail.com>
2014-05-04 22:53:07 +0900

    A minor change.

--- .emacs.d/init-loader/02-looks.el
+++ .emacs.d/init-loader/02-looks.el
@@ -1,14 +1,14 @@
 ;; 透明化
 (add-to-list 'default-frame-alist '(alpha . (0.9 0.9)))
 
+;; Powerline
+(when (require 'powerline nil t)
+  (powerline-default-theme))
+
 ;; 色
 (when (require 'color-theme nil t)
   (color-theme-initialize)
   (color-theme-dark-laptop))
-
-;; Powerline
-(when (require 'powerline nil t)
-  (powerline-default-theme))
 
 ;; フェイス設定例
 ;; (custom-set-faces
