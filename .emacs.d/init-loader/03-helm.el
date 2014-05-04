3563b60fe7fcba822a3a63a42186b5f7fde6c7e2
nishiyama_y <allegllet.scherzand@gmail.com>
2014-05-04 22:52:40 +0900

    Modify key maps.

--- .emacs.d/init-loader/03-helm.el
+++ .emacs.d/init-loader/03-helm.el
@@ -1,10 +1,17 @@
-(require 'helm-config)
+(when (require 'helm-config nil t)
+  (helm-mode 1)
 
-(helm-mode 1)
+  (global-set-key (kbd "M-x") 'helm-M-x)
+  (global-set-key (kbd "C-x C-f") 'helm-find-files)
+  (global-set-key (kbd "M-y") 'helm-show-kill-ring)
+  (global-set-key (kbd "C-x b") 'helm-buffers-list)
+  (global-set-key (kbd "C-;") 'helm-mini)
+  (global-set-key (kbd "C-c d") 'helm-descbinds)
 
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-x b") 'helm-buffers-list)
-(global-set-key (kbd "C-;") 'helm-mini)
-(global-set-key (kbd "C-c d") 'helm-descbinds)
+  ;; mini-bufferでC-hがprefix commandとして機能してしまうので上書き。
+  (define-key helm-map (kbd "C-h") 'delete-backward-char)
+  (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+  
+  ;; mini-bufferでのデフォルトのTABの挙動がActionの表示なので、補完に上書き。
+  (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+  (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action))
