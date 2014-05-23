4333bfa2d92f8cc8e546ec1b8c36e2b6aaad3981
nishiyama_y <allegllet.scherzand@gmail.com>
2014-05-23 14:53:01 +0900

    Configure smooth-scroll.

--- .emacs.d/init-loader/04-smooth-scroll.el
+++ .emacs.d/init-loader/04-smooth-scroll.el
@@ -1,2 +1,4 @@
 (when (require 'smooth-scroll nil t)
-  (smooth-scroll-mode t))
+  (smooth-scroll-mode t)
+  (custom-set-variables '(smooth-scroll/vscroll-step-size 8))
+  (custom-set-variables '(smooth-scroll/hscroll-step-size 8)))
