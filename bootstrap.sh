30742c1eb102ddd83a7c9d28abe051fdb91cce3a
yuseinishiyama <allegllet.scherzand@gmail.com>
2014-07-25 14:51:41 +0900

    Install dotfiles automatically.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-DOT_FILES=( .emacs.d .zshrc .bash_profile .gitconfig .tmux.conf .peco)
+# create symlinks
+symlink() {
+    echo "Create symlinks..."
+    DOT_FILES=( .emacs.d .zshrc .bash_profile .gitconfig .tmux.conf .peco)
 
-for file in ${DOT_FILES[@]}
-do
-    ln -s $HOME/.dotfiles/$file $HOME/$file
-done
+    for file in ${DOT_FILES[@]}
+    do
+        ln -s $HOME/.dotfiles/$file $HOME/$file
+    done
+}
+
+brew_setup() {
+    echo "Setup homebrew..."
+    # Install homebrew
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+    brew bundle Brewfile
+    brew bundle Caskfile
+    brew cask alfred link
+}
+
+symlink
+brew_setup
+# Set zsh as default shell.
+chsh -s `which zsh`
+echo "finish!"
+
+# TODO: Settings for mac
+
--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# How to get started.
+# sh -c "`curl -fsSL https://raw.github.com/yuseinishiyama/dotfiles/master/install.sh`"
+
 if [ ! -d "$HOME/.dotfiles" ]; then
     echo "Installing dotfiles..."
     git clone https://github.com/yuseinishiyama/dotfiles.git "$HOME/.dotfiles"
-    cd "$HOME/.dotfiles"
     source bootstrap.sh
 else
     echo "dotfiles are already installed."
