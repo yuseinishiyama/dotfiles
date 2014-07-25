a6981b14efb1491ec61c356d5819b0584d583b84
yuseinishiyama <allegllet.scherzand@gmail.com>
2014-07-25 19:52:23 +0900

    Add some todo comments to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,13 +17,12 @@ brew_setup() {
     ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
     brew bundle $HOME/.dotfiles/Brewfile
     brew bundle $HOME/.dotfiles/Caskfile
-    brew cask alfred link
+    brew cask alfred link # TODO: 失敗する
 }
-
+# TODO: oh-my-zsh
 symlink
 brew_setup
-# Set zsh as default shell.
-chsh -s `which zsh`
+chsh -s `which zsh` # Set zsh as default shell.
 echo "finish!"
 
 # TODO: Settings for mac
