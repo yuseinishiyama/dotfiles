0c7d9f0a20214d72c6bb4086e903bd902eadbc65
yuseinishiyama <allegllet.scherzand@gmail.com>
2015-02-19 15:24:37 +0900

    update helm

--- .emacs.d/init-loader/03-helm.el
+++ .emacs.d/init-loader/03-helm.el
@@ -1,13 +1,6 @@
 (when (require 'helm-config nil t)
   (helm-mode 1)
 
-  (global-set-key (kbd "M-x") 'helm-M-x)
-  (global-set-key (kbd "C-x C-f") 'helm-find-files)
-  (global-set-key (kbd "M-y") 'helm-show-kill-ring)
-  (global-set-key (kbd "C-x b") 'helm-buffers-list)
-  (global-set-key (kbd "C-;") 'helm-mini)
-  (global-set-key (kbd "C-c d") 'helm-descbinds)
-
   ;; mini-bufferでC-hがprefix commandとして機能してしまうので上書き。
   (define-key helm-map (kbd "C-h") 'delete-backward-char)
   (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
