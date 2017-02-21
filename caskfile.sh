e9f95d032b42abd5eeeea1241fd1ac09eb5ffab0
yuseinishiyama <allegllet.scherzand@gmail.com>
2017-02-21 16:27:12 +0000

    Clean caskfile.sh up

--- caskfile.sh
+++ caskfile.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 brew tap caskroom/cask
-brew tap caskroom/versions
-brew install brew-cask
 
 brew cask install alfred
 brew cask install atom
 brew cask install appcleaner
 brew cask install clipmenu
-brew cask install dash
+brew cask install karabiner-elements
 brew cask install dropbox
 brew cask install emacs
 brew cask install evernote
@@ -20,8 +18,7 @@ brew cask install sequel-pro
 brew cask install skype
 brew cask install slack
 brew cask install spectacle
-brew cask install yorufukurou
-
-brew cask alfred link
+brew cask install totalfinder
+brew cask install zeplin
 
 brew cleanup
