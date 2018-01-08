df6c44cfb12c5242b553d75fdf8bb650e3a3b350
Yusei Nishiyama <yusei-nishiyama@cookpad.com>
2018-01-08 22:11:37 +0900

    Install Xcode command line tools via softwareupdate

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 if [ ! -d "$HOME/.dotfiles" ]; then
-    echo "Installing..."
+    echo "Installing Xcode CLI tools..."
+    touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+    PROD=$(softwareupdate -l |
+      grep "\*.*Command Line" |
+      head -n 1 | awk -F"*" '{print $2}' |
+      sed -e 's/^ *//' |
+      tr -d '\n')
+    softwareupdate -i "$PROD" --verbose;
 
     install_dir=.dotfiles
     cd $HOME
