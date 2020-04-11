0a37301ba4ed08f25039d6d67f0838cfee0bc19b
Yusei Nishiyama <allegllet.scherzand@gmail.com>
2020-04-11 02:56:08 +0200

    Update install location

--- install.sh
+++ install.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
 
-if [ ! -d "$HOME/.dotfiles" ]; then
-    echo "Installing Xcode CLI tools..."
-    touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-    PROD=$(softwareupdate -l |
-      grep "\*.*Command Line" |
-      head -n 1 | awk -F"*" '{print $2}' |
-      sed -e 's/^ *//' |
-      tr -d '\n')
-    softwareupdate -i "$PROD" --verbose;
-
-    install_dir=.dotfiles
-    cd $HOME
-    git clone https://github.com/yuseinishiyama/dotfiles.git $install_dir
-    cd $install_dir
-    ./bin/run i
-else
-    echo "dotfiles are already installed"
-fi
+git clone https://github.com/yuseinishiyama/dotfiles.git --depth=1
+./dotfiles/bin/bootstrap
+rm -rf dotfiles
