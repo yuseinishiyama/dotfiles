b5102d0dc77020e131a973d04ab08d6d3f930f48
Yusei Nishiyama <yusei.nishiyama@soundcloud.com>
2020-04-12 18:32:55 +0200

    Merge bootstrap into install

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
 git clone https://github.com/yuseinishiyama/dotfiles.git --depth=1
-./dotfiles/bin/bootstrap
+
+source dotfiles/lib/brew
+source dotfiles/lib/dev_tools
+
+# Re-clone into designated place
+install_commandline_tools
+install_brew
+brew install ghq
+GHQ_ROOT=~/.ghq ghq get https://github.com/yuseinishiyama/dotfiles.git
+
+# Main procedure
+$(ghq root)/$(ghq list | grep yuseinishiyama/dotfiles)/bin/run import
+
+# Clean up
 rm -rf dotfiles
 echo 'dotfile is installed successfully. Restart the terminal'
