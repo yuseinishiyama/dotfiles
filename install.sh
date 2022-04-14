14a5a2fe1de16561ebc2b7a0cb0bbd21599f1b4a
Yusei Nishiyama <me@yuseinishiyama.com>
2022-04-14 12:13:04 +0200

    Install commandline tools directly in installer
    Signed-off-by: Yusei Nishiyama <me@yuseinishiyama.com>

--- install.sh
+++ install.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
+install_commandline_tools() {
+    touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+    PROD=$(softwareupdate -l |
+      grep "\*.*Command Line" |
+      head -n 1 | awk -F"*" '{print $2}' |
+      sed -e 's/^ *//' |
+      tr -d '\n')
+    softwareupdate -i "$PROD" --verbose;
+}
+
+install_commandline_tools
+
 git clone https://github.com/yuseinishiyama/dotfiles.git --depth=1
 
 source dotfiles/lib/brew
-source dotfiles/lib/dev_tools
 
 # Re-clone into designated place
-install_commandline_tools
 install_brew
 brew install ghq
 GHQ_ROOT=~/.ghq ghq get https://github.com/yuseinishiyama/dotfiles.git
